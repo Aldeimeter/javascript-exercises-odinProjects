c9aceb201ab1dc3d6e1a299f456ee42665be61d5
Artem Zaitsev <Aldeimeter666@gmail.com>
2025-01-29 15:54:58 +0100

    complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  let sum = 0;
+  if (Number.isInteger(a) && Number.isInteger(b) && a > 0 && b > 0) {
+    const startingNumber = a < b ? a : b;
+    const endingNumber = a > b ? a : b;
+    for (let i = startingNumber; i <= endingNumber; i++) {
+      sum += i;
+    }
+    return sum;
+  }
 
+  return "ERROR";
 };
 
 // Do not edit below this line
