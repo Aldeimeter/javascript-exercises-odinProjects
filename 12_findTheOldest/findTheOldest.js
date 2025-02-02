da211ec67c2846ccc9064cede07206c998b8433c
Artem Zaitsev <Aldeimeter666@gmail.com>
2025-02-03 00:26:25 +0100

    complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  function getAge(birth, death) {
+    if (!death) {
+      death = new Date().getFullYear();
+    }
+    return death - birth;
+  }
+  return people
+    .map((person) => ({
+      ...person,
+      age: getAge(person.yearOfBirth, person.yearOfDeath),
+    }))
+    .sort((a, b) => b.age - a.age)[0];
 };
 
 // Do not edit below this line
