a8921ca886643a98f313468228ea61b906397ca4
Artem Zaitsev <Aldeimeter666@gmail.com>
2025-02-03 00:02:08 +0100

    11_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  const parsedN = Number(n);
+  if (parsedN < 0) return "OOPS";
+  const arr = [0, 1, 1];
+  for (let i = 3; i <= parsedN; i++) {
+    arr[i] = arr[i - 2] + arr[i - 1];
+  }
 
+  return arr[parsedN];
 };
 
 // Do not edit below this line
