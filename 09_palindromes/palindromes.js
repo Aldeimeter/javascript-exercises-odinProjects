c58611083c5101dd3f087e6e6245d112937bf67b
Artem Zaitsev <Aldeimeter666@gmail.com>
2025-02-02 23:41:09 +0100

    complete 09_palindomes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const cleanedWord = str
+    .toLowerCase()
+    .split("")
+    .filter((char) => /[a-z0-9]/.test(char))
+    .join("");
+  return cleanedWord === cleanedWord.split("").reverse().join("");
 };
 
 // Do not edit below this line
